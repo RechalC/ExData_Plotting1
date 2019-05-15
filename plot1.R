667575f919cd7f928681b2fab33abbb2db4b2a78
RechalC <44951624+RechalC@users.noreply.github.com>
2019-05-15 18:28:59 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,18 @@
+setwd("E:/rechal/Coursera/DataScienceSpecialization/4-Exploratory Data Analysis/Week 1/exdata_data_household_power_consumption")
+
+library(dplyr)
+library(lubridate)
+
+# reading relevant data
+data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+
+# clearing name row and unrelevant variables, creating numerics
+data <- data %>% select(V3) %>% mutate(V3 = as.numeric(as.character(V3)))
+
+
+png("plot1.png", width=480, height=480)
+
+# create histogram
+hist(data$V3, col = "red", main = "Global Active Power", xlab = "Global Active Power (killowatts)", ylab = "Frequency")
+
+dev.off()
